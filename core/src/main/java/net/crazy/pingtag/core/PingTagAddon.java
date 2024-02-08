8839c5cf15c9b8befdeed996485096f71c7828d3
MisterCore <Heiko-YouTube@web.de>
2024-02-08 22:05:32 +0100

    Added Setting to change Position (above, below name) and scale of the tag

--- core/src/main/java/net/crazy/pingtag/core/PingTagAddon.java
+++ core/src/main/java/net/crazy/pingtag/core/PingTagAddon.java
@@ -1,5 +1,6 @@
 package net.crazy.pingtag.core;
 
+import net.crazy.pingtag.core.PingTagConfiguration.Position;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.client.entity.player.tag.PositionType;
 import net.labymod.api.models.addon.annotation.AddonMain;
@@ -7,18 +8,33 @@ import net.labymod.api.models.addon.annotation.AddonMain;
 @AddonMain
 public class PingTagAddon extends LabyAddon<PingTagConfiguration> {
 
+  private static PingTagAddon instance;
+
   @Override
   protected void enable() {
     this.registerSettingCategory();
+    instance = this;
 
-    PingTag tag = PingTag.create(this);
-    labyAPI().tagRegistry().registerAfter("pingtag", "badge", PositionType.ABOVE_NAME, tag);
+    registerTag();
 
     this.logger().info("PingTag | Addon enabled.");
+  }
+
+  public void registerTag() {
+    if(this.configuration().getPosition().get() == Position.ABOVE) {
+      labyAPI().tagRegistry().registerAfter("badge", "pingtag", PositionType.ABOVE_NAME, new PingTag(this));
+    } else {
+      labyAPI().tagRegistry().register("pingtag", PositionType.BELOW_NAME, new PingTag(this));
+    }
+    logger().info("Registered...");
   }
 
   @Override
   protected Class<PingTagConfiguration> configurationClass() {
     return PingTagConfiguration.class;
+  }
+
+  public static PingTagAddon getInstance() {
+    return instance;
   }
 }
--- core/src/main/java/net/crazy/pingtag/core/PingTagConfiguration.java
+++ core/src/main/java/net/crazy/pingtag/core/PingTagConfiguration.java
@@ -1,14 +1,30 @@
 package net.crazy.pingtag.core;
 
+import net.labymod.api.Laby;
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownEntryTranslationPrefix;
+import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.configuration.settings.annotation.SettingRequires;
 
 @ConfigName("settings")
 public class PingTagConfiguration extends AddonConfig {
+
+  public PingTagConfiguration() {
+    this.position.addChangeListener(tagPosition -> {
+      if(!enabled().get()) return;
+      if(PingTagAddon.getInstance() != null) {
+        Laby.labyAPI().tagRegistry().unregister("pingtag");
+        PingTagAddon.getInstance().logger().info("Unregistered...");
+        PingTagAddon.getInstance().registerTag();
+      }
+    });
+    this.customFormat.addChangeListener(ignored -> PingTag.updateCustomFormat(this.customFormat));
+  }
 
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
@@ -19,6 +35,13 @@ public class PingTagConfiguration extends AddonConfig {
   @TextFieldWidget.TextFieldSetting
   @SettingRequires(value = "coloured", invert = true)
   private final ConfigProperty<String> customFormat = new ConfigProperty<>("%ping%ms");
+
+  @DropdownSetting
+  @DropdownEntryTranslationPrefix("pingtag.settings.position.type")
+  private final ConfigProperty<Position> position = new ConfigProperty<>(Position.ABOVE);
+
+  @SliderSetting(min = 0.4F, max = 1.5F, steps = 0.1F)
+  private final ConfigProperty<Float> scale = new ConfigProperty<>(1.0F);
 
   @Override
   public ConfigProperty<Boolean> enabled() {
@@ -32,4 +55,17 @@ public class PingTagConfiguration extends AddonConfig {
   public ConfigProperty<String> getCustomFormat() {
     return customFormat;
   }
+
+  public ConfigProperty<Position> getPosition() {
+    return position;
+  }
+
+  public ConfigProperty<Float> getScale() {
+    return scale;
+  }
+
+  public enum Position {
+    ABOVE, BELOW
+  }
+
 }
